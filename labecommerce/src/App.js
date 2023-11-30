ab2dc1d491f9a02cb78c5d1c322c77b7ebc05039
Lucas-Otavio-Pinesso-de-Morais <lucaspinessomorais@gmail.com>
2023-11-30 02:24:33 -0300

    [funcionalidade] Implementa Inputs controlados no select

--- labecommerce/src/App.js
+++ labecommerce/src/App.js
@@ -4,8 +4,22 @@ import { Cart } from './Components/ShoppingCart/Cart/Cart';
 import { GlobalStyle } from './GlobalStyle';
 import { AppContainer } from './AppStyle'
 import { productsList } from './assents/productsList'
+import { useState } from 'react'
 
 function App() {
+  const [minFilter, setMinFilter] = useState(0)
+  const [maxFilter, setMaxFilter] = useState(0)
+  const [searchFilter, setSearchFilter] = useState("")
+  const [cart, setCart] = useState("")
+  const [amount, setAmount] = useState(0)
+
+  const handleMinFilter  = (event) => setMinFilter(event.target.value)
+  const handleMaxFilter  = (event) => setMaxFilter(event.target.value)
+  const handleSearchFilter  = (event) => setSearchFilter(event.target.value)
+  const handleCart  = (event) => setCart(event.target.value)
+  const handleAmount  = (event) => setAmount(event.target.value)
+  
+
   return (
     <AppContainer>
       <GlobalStyle />
--- labecommerce/src/Components/ProductList/Home/Home.js
+++ labecommerce/src/Components/ProductList/Home/Home.js
@@ -1,11 +1,21 @@
 import React from "react";
 import { CardConteiner, QuantidadeProdutosConteiner, HomeStyle, OrdenaçãoContainer } from "./HomeStyle";
 import { ProductCard } from "../ProductCard/ProductCard";
+import { useState } from "react"
 
 export function Home (props) {
     const { productsList } = props
 
-    const renderList = productsList.map(product =>{
+    const [ordination, setOrdination] = useState("")
+
+    const handleOrdination  = (event) => setOrdination(event.target.value)
+
+    const renderList = productsList
+    .sort((a, b) => ordination === "" || ordination === "z-a" && a.name > b.name ? 1 : -1)
+    .sort((a, b) => ordination === "" || ordination === "a-z" && a.name > b.name ? -1 : 1)
+    .sort((a, b) => ordination === "" || ordination === "crescente" && a.value > b.value ? 1 : -1)
+    .sort((a, b) => ordination === "" || ordination === "decrescente" && a.value > b.value ? -1 : 1)
+    .map(product => {
         return (
             <ProductCard
                     key={product.id}
@@ -24,9 +34,12 @@ export function Home (props) {
                 <OrdenaçãoContainer>
                     Ordenação:
 
-                    <select name="Ordenação:">
-                        <option value="crescente">Crescente</option>
-                        <option value="decrescente">Decrescente</option>
+                    <select value={ordination} onChange={handleOrdination}>
+                        <option value="default">Selecione uma opção</option>
+                        <option value="a-z">Nome: de A até Z</option>
+                        <option value="z-a">Nome: de Z até A</option>
+                        <option value="crescente">Preço: Crescente</option>
+                        <option value="decrescente">Preço: Decrescente</option>
                     </select>
                 </OrdenaçãoContainer>
             </QuantidadeProdutosConteiner>
